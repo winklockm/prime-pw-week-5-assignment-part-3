bb7a9a51f6800257f73c157921410d8cf6c9fde0
Maggie Whitlock <maggie.whitlock@gmail.com>
2022-08-08 12:56:54 -0500

    add 6 albums to collection

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -12,5 +12,10 @@ function addToCollection(title, artist, yearPublished)  {
     return album;
 } //end addToCollection
 
-console.log('added to collection', addToCollection('Is This It', 'The Strokes', 2001));
-console.log(collection);
+console.log('add first to collection', addToCollection('Is This It', 'The Strokes', 2001));
+console.log('add second album to collection', addToCollection('Bloodnstuff', 'Bloodnstuff', 2015));
+console.log('add third album to collection', addToCollection('Manipulator', 'Ty Segall', 2014));
+console.log('add fourth album to collection', addToCollection('Modern Country', 'William Tyler', 2016));
+console.log('add fifth album to collection', addToCollection('Most Known Unknown', 'Three 6 Mafia', 2005));
+console.log('add sixth album to collection', addToCollection('Aja', 'Steely Dan', 1977));
+console.log('My collection is:', collection);
