cd774848e317d17b9d70dc6f2a8d1653e3f2d0c6
Maggie Whitlock <maggie.whitlock@gmail.com>
2022-08-08 18:34:29 -0500

    successful attempt at findByArtist function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -19,14 +19,9 @@ function findByArtist(artist) {
         if(collection[i].albumArtist === artist){
             searchResults.push(collection[i]);
         }
-        // else{
-        //     //console.log('no search results:');
-        //     searchResults.length = 0;
-        // }
     }
     return searchResults;
 }// end findByArtist
-
 
 function showCollection(array)   {
     console.log('number of albums in my collection:', array.length);
@@ -35,16 +30,16 @@ function showCollection(array)   {
     }
 } //end showCollection
 
-
 console.log('add first to collection', addToCollection('Is This It', 'The Strokes', 2001));
 console.log('add second album to collection', addToCollection('Bloodnstuff', 'Bloodnstuff', 2015));
 console.log('add third album to collection', addToCollection('Manipulator', 'Ty Segall', 2014));
 console.log('add fourth album to collection', addToCollection('Modern Country', 'William Tyler', 2016));
-console.log('add fifth album to collection', addToCollection('Most Known Unknown', 'Three 6 Mafia', 2005));
+console.log('add fifth album to collection', addToCollection('Pretzel Logic', 'Steely Dan', 1974));
 console.log('add sixth album to collection', addToCollection('Aja', 'Steely Dan', 1977));
 console.log('My collection is:', collection);
 
 console.log(showCollection(collection));
 
-console.log('searching for Bloodnstuff', findByArtist('Bloodnstuff'));
-console.log('searching for Ludacris', findByArtist('Bloodnstuff'));
+console.log('search: Steely Dan', findByArtist('Steely Dan'));
+console.log('searching: Ludacris', findByArtist('Three 6 Mafia'));
+console.log('searching: William Tyler', findByArtist('William Tyler'));
