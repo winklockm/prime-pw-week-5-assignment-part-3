13e9b5da545fde48453a0b99925c72661cda0eef
Maggie Whitlock <maggie.whitlock@gmail.com>
2022-08-08 12:01:52 -0500

    create addToCollection function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,3 +1,16 @@
 console.log('***** Music Collection *****')
 
 let collection = [];
+
+function addToCollection(title, artist, yearPublished)  {
+    let album = {
+        albumTitle: title,
+        albumArtist: artist,
+        albumYearPublished: yearPublished
+    } //end album
+    collection.push(album);
+    return album;
+} //end addToCollection
+
+console.log('added to collection', addToCollection('Is This It', 'The Strokes', 2001));
+console.log(collection);
